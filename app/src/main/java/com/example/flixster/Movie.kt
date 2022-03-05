ccbdfa7137eb01a3db31622336ea57657320ece9
Hongkun Zeng <hzeng21@illinois.edu>
2022-03-05 00:28:36 -0600

    flixster part 2 completed

--- app/src/main/java/com/example/flixster/Movie.kt
+++ app/src/main/java/com/example/flixster/Movie.kt
@@ -1,12 +1,18 @@
 package com.example.flixster
 
+import android.os.Parcelable
+import kotlinx.parcelize.IgnoredOnParcel
+import kotlinx.parcelize.Parcelize
 import org.json.JSONArray
 
+@Parcelize
 class Movie(val movieId: Int,
+            val voteAverage: Double,
             private val posterPath: String,
             val title: String,
             val overview: String,
-) {
+) : Parcelable {
+    @IgnoredOnParcel
     val posterImageUrl = "https://image.tmdb.org/t/p/w342/$posterPath"
     companion object{
         fun fromJsonArray(movieJsonArray: JSONArray): List<Movie> {
@@ -16,6 +22,7 @@ class Movie(val movieId: Int,
                 movies.add(
                     Movie(
                         movieJson.getInt("id"),
+                        movieJson.getDouble("vote_average"),
                         movieJson.getString("poster_path"),
                         movieJson.getString("title"),
                         movieJson.getString("overview")
